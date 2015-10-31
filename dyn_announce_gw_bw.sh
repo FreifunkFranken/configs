511b441e011f4ad3356d33c1e680ad97aa9459e2
green27 <koe_fue@gmx.de>
2015-10-31 09:57:43 +0100

    Create dyn_announce_gw_bw.sh

---
+++ dyn_announce_gw_bw.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ 
+if [ -z "$1" ]; then
+        echo
+        echo "usage: $0 <network-interface> <update_interval [sec]> <total BW up [Mbit/sec]> <total BW down [Mbit/sec]>"
+        echo
+        echo "e.g. $0 eth0 60 10 10"
+        echo
+        exit
+fi
+
+while true
+do
+        # Bandwidth currently used (time averaged)
+        R1=$(cat "/sys/class/net/$1/statistics/rx_bytes")
+        T1=$(cat "/sys/class/net/$1/statistics/tx_bytes")
+        sleep "$2"
+        R2=$(cat "/sys/class/net/$1/statistics/rx_bytes")
+        T2=$(cat "/sys/class/net/$1/statistics/tx_bytes")
+        TkbitPS=$(echo "scale=0; ($T2 - $T1) / 1024 * 8 / $2" | bc -l)
+        RkbitPS=$(echo "scale=0; ($R2 - $R1) / 1024 * 8 / $2" | bc -l)
+#        echo "BW used      -- up $1: $TkbitPS kBit/s; down $1: $RkbitPS kBit/s"
+
+        # Remaining bandwidth available; cut-off negative values
+        Tavail_kbitPS=$(echo "scale=0; if (($3 * 1024 - $TkbitPS) >0) ($3 * 1024 - $TkbitPS) else 0" | bc -l)
+        Ravail_kbitPS=$(echo "scale=0; if (($4 * 1024 - $RkbitPS) >0) ($4 * 1024 - $RkbitPS) else 0" | bc -l)
+#        echo "BW available -- up $1: $Tavail_kbitPS kBit/s; down $1: $Ravail_kbitPS kBit/s"
+
+        # Pass calculated figures to B.A.T.M.A.N Gateway Selection
+#        echo "batctl gw_mode server ${Ravail_kbitPS}kbit/${Tavail_kbitPS}kbit"
+        batctl gw_mode server "${Ravail_kbitPS}kbit/${Tavail_kbitPS}kbit" 
+done
